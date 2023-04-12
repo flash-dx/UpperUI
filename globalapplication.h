bcd566d6e69decebac4ca9777615c0a6f4545443
YZH-PC <YZH-PC@flashdx.co>
2023-04-12 17:34:36 +0800

    s

--- globalapplication.h
+++ globalapplication.h
@@ -1,18 +1,16 @@
 #ifndef GLOBALAPPLICATION_H
 #define GLOBALAPPLICATION_H
 #include <QApplication>
-//#include "module/exglobal.h"
 #include <QTimer>
+
 class GlobalApplication : public QApplication
 {
 public:
     GlobalApplication(int &argc,char **argv);
     ~GlobalApplication();
     bool notify(QObject*,QEvent *);
-    void setWindowInstance(QWidget *wnd);
-    //ExGlobal *global;
+
 private:
-    QWidget *widget;
     int timerid;
     int timerkey;
 
